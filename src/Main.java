c6261c241f17567edadb1e1252813ff3292fbb4f
vnavacar <vnavacar@myuax.com>
2022-10-19 15:26:44 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            int number = scanner.nextInt();
+            System.out.println("The number is: " + number);
+        }
+    
+}
