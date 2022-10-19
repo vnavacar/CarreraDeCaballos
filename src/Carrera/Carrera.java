660fe9395bb761ca94fea89e2eb7ad9747ab9eef
vnavacar <vnavacar@myuax.com>
2022-10-19 15:31:45 +0200

    cambios

---
+++ src/Carrera/Carrera.java
@@ -0,0 +1,10 @@
+package Carrera;
+import java.util.Scanner;
+
+public class Carrera {
+    private static int numeroCaballos;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Introduzca el numero de caballos: ");
+    int numero = scanner.nextInt();
+    System.out.println("El numero de caballos sera: " + numero);
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
+package Carrera;
 
-
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Main {
     
         public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter a number: ");
-            int number = scanner.nextInt();
-            System.out.println("The number is: " + number);
+
         }
     
 }
